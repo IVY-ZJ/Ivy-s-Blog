3239f50a3cfd8928fcd9379adb483689fcaaa2f7
IVY <ivyzj2000@gmail.com>
2026-08-10 20:54:10 +0800

    feat:音乐和liquid glass更新“

--- assets/js/tracks.js
+++ assets/js/tracks.js
@@ -5,5 +5,6 @@ window.SITE_TRACKS = [
   { title: "Eutopia",    src: "music/Eutopia.flac" },
   { title: "卡农",        src: "music/%E5%8D%A1%E5%86%9C.mp3" },
   { title: "夜的钢琴曲5",  src: "music/%E5%A4%9C%E7%9A%84%E9%92%A2%E7%90%B4%E6%9B%B25.mp3" },
-  { title: "菊次郎的夏天", src: "music/%E8%8F%8A%E6%AC%A1%E9%83%8E%E7%9A%84%E5%A4%8F%E5%A4%A9.mp3" }
+  { title: "菊次郎的夏天", src: "music/%E8%8F%8A%E6%AC%A1%E9%83%8E%E7%9A%84%E5%A4%8F%E5%A4%A9.mp3" },
+  { title: "Flower Dance", src: "music/Flower%20Dance.flac" }
 ];
