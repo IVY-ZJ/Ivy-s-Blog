42a0551ed3ebcc273e30370bb4177beb83119c3d
IVY <ivyzj2000@gmail.com>
2026-08-12 20:04:43 +0800

    feat:删减音乐

--- assets/js/tracks.js
+++ assets/js/tracks.js
@@ -3,7 +3,6 @@
 // music.html 与首页右侧栏 mini 播放器都从这里读取，保证两端始终同步。
 window.SITE_TRACKS = [
   { title: "Eutopia",    src: "music/Eutopia.mp3" },
-  { title: "卡农",        src: "music/%E5%8D%A1%E5%86%9C.mp3" },
   { title: "夜的钢琴曲5",  src: "music/%E5%A4%9C%E7%9A%84%E9%92%A2%E7%90%B4%E6%9B%B25.mp3" },
   { title: "菊次郎的夏天", src: "music/%E8%8F%8A%E6%AC%A1%E9%83%8E%E7%9A%84%E5%A4%8F%E5%A4%A9.mp3" },
   { title: "Flower Dance", src: "music/Flower%20Dance.mp3" }
